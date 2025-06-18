f504075c952ff58c095f5dd0c39cf4a4c1b6965d
amitSteinmetz <117719977+amitSteinmetz@users.noreply.github.com>
2025-06-18 22:32:51 +0300

    css

--- src/components/main/Section.tsx
+++ src/components/main/Section.tsx
@@ -1,10 +1,12 @@
 import CardRenderer from "./cards/CardRenderer";
 import { SectionModel } from "../../models/section.model";
 import { GoTriangleDown } from "react-icons/go";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import webIcon from "../../assets/images/web-icon.png";
 
 const Section = ({ sectionData }: { sectionData: SectionModel }) => {
+  const [showSectionLinks, setShowSectionLinks] = useState(false);
+
   function renderCards(arr) {
     return arr.map((card, index: number) => (
       <CardRenderer
@@ -15,14 +17,14 @@ const Section = ({ sectionData }: { sectionData: SectionModel }) => {
       ></CardRenderer>
     ));
   }
-
   // useEffect(() => {
-  //   document.body.style.overflow = isModalOpen ? "hidden" : "auto";
-  // }, [isModalOpen]);
-
-  useEffect(() => {
-    document.body.style.overflow = "hidden";
-  }, []);
+  //   if (
+  //     document.body.style.overflow === "hidden" ||
+  //     parseInt(document.body.style.width) < 500
+  //   ) {
+  //     document.body.style.overflow = showSectionLinks ? "hidden" : "auto";
+  //   }
+  // }, [showSectionLinks]);
 
   return (
     <div
@@ -32,33 +34,49 @@ const Section = ({ sectionData }: { sectionData: SectionModel }) => {
         <h1 className="section-header__title font-xl">{sectionData.title}</h1>
 
         {sectionData.relatedLinks.length > 0 && (
-          <div className="section-header__subtitle">
+          <div
+            className="section-header__subtitle"
+            onClick={() => {
+              setShowSectionLinks(!showSectionLinks);
+            }}
+            onMouseLeave={() => {
+              setShowSectionLinks(false);
+            }}
+          >
             <span className="section-header__seperator">|</span>
             לינקים בנושא
             <GoTriangleDown className="section-header__arrowBtn" />
-            <div className="subtitle__links-container">
-              <div className="subtitle__links">
-                <div className="subtitle__links_line-symbol"></div>
+            {showSectionLinks && (
+              <div className="subtitle__links-container">
+                <div className="subtitle__links">
+                  <div className="subtitle__links_line-symbol"></div>
 
-                <div className="subtitle__links_title bold font-base">
-                  לינקים בנושא
-                </div>
-
-                {sectionData.relatedLinks.map((link) => (
-                  <div className={`links__item ${link.id === 1 && "first-link"}`}>
-                    <img className="web-icon" src={webIcon} alt="" />
-
-                    <div className="links__item_content">
-                      <div className="links__item_title bold font-sm">{link.title}</div>
-                      <div className="links__item_subtitle font-xs">
-                        {link.subTitle}
-                      </div>
-                      <a href={link.url} className="links__item_url font-xs">{link.name}</a>
-                    </div>
+                  <div className="subtitle__links_title bold font-base">
+                    לינקים בנושא
                   </div>
-                ))}
+
+                  {sectionData.relatedLinks.map((link) => (
+                    <div
+                      className={`links__item ${link.id === 1 && "first-link"}`}
+                    >
+                      <img className="web-icon" src={webIcon} alt="" />
+
+                      <div className="links__item_content">
+                        <div className="links__item_title bold font-sm">
+                          {link.title}
+                        </div>
+                        <div className="links__item_subtitle font-xs">
+                          {link.subTitle}
+                        </div>
+                        <a href={link.url} className="links__item_url font-xs">
+                          {link.name}
+                        </a>
+                      </div>
+                    </div>
+                  ))}
+                </div>
               </div>
-            </div>
+            )}
           </div>
         )}
       </div>
