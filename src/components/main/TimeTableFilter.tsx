b34d2c777216a433aa2adb6820648b8592977ab6
amitSteinmetz <117719977+amitSteinmetz@users.noreply.github.com>
2025-06-23 16:13:39 +0300

    finish graphs

---
+++ src/components/main/TimeTableFilter.tsx
@@ -0,0 +1,66 @@
+import { useContext, useState } from "react";
+import { DataContext } from "../../contexts/DataContext";
+import { IoIosArrowDown } from "react-icons/io";
+
+const TimeTableFilter = ({ sectionId, cardId }) => {
+  const { onChangeGraphDataTimeRange } = useContext(DataContext);
+  const [showTable, setShowTable] = useState(false);
+  const timeRanges = [
+    { key: "lastMonth", value: "last-month", valueInHebrew: "חודש אחרון" },
+    { key: "last3Months", value: "last-3-months", valueInHebrew: "3 חודשים" },
+    { key: "last6Months", value: "last-6-months", valueInHebrew: "6 חודשים" },
+    { key: "lastYear", value: "last-year", valueInHebrew: "שנה" },
+    { key: "all", value: "all", valueInHebrew: "עד עכשיו" },
+  ];
+  const [currentTimeRange, setCurrentTimeRange] = useState(timeRanges[0]);
+  const [selectedTimeRange, setSelectedTimeRange] = useState(timeRanges[0]);
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    setSelectedTimeRange(currentTimeRange);
+    onChangeGraphDataTimeRange(sectionId, cardId, selectedTimeRange.value);
+    setShowTable(false);
+  }
+
+  return (
+    <div className="filter-table">
+      <div
+        className="table__current font-sm"
+        onClick={() => setShowTable(!showTable)}
+      >
+        <span>{selectedTimeRange.valueInHebrew}</span>
+        <IoIosArrowDown className="font-sm" />
+      </div>
+
+      {showTable && (
+        <form onSubmit={handleSubmit} className="filter-table__form">
+          <div className="form-title bold font-sm">זמן</div>
+
+          <div className="form-inputs">
+            {timeRanges.map((range) => (
+              <div className="form-input font-sm" key={range.key}>
+                <input
+                  type="radio"
+                  checked={currentTimeRange.value === range.value}
+                  onChange={() => {
+                    setCurrentTimeRange(range);
+                  }}
+                />
+                <label>{range.valueInHebrew}</label>
+              </div>
+            ))}
+          </div>
+
+          <div className="form-buttons">
+            <button type="submit">אישור</button>
+            <button type="button" onClick={() => setShowTable(false)}>
+              ביטול
+            </button>
+          </div>
+        </form>
+      )}
+    </div>
+  );
+};
+
+export default TimeTableFilter;
--- src/components/main/cards/GraphicalCard.tsx
+++ src/components/main/cards/GraphicalCard.tsx
@@ -2,23 +2,309 @@ import ReactECharts from "echarts-for-react";
 import { moreInfoBtn } from "../../../assets/svgs";
 import { FiMoreVertical } from "react-icons/fi";
 import { GraphicalCardModel } from "../../../models/card.model";
-import { useContext, useState } from "react";
-import { DataContext } from "../../../contexts/DataContext";
+import { useState } from "react";
 import { CiShare2 } from "react-icons/ci";
 import { IoIosArrowRoundDown } from "react-icons/io";
+import TimeTableFilter from "../TimeTableFilter";
 
 const GraphicalCard = ({ sectionId, card, hasContainerParent }) => {
   const graphicalCard: GraphicalCardModel = card as GraphicalCardModel;
-  const options = JSON.parse(graphicalCard.options);
-  const { onChangeGraphDataTimeRange } = useContext(DataContext);
-  const timeRanges = {
-    lastMonth: "last-month",
-    last3Months: "last-3-months",
-    last6Months: "last-6-months",
-    lastYear: "last-year",
-    all: "all",
-  };
+  // const options = JSON.parse(graphicalCard.options);
   const [showCardActions, setShowCardActions] = useState(false);
+
+  const options = {
+    tooltip: {
+      trigger: "axis",
+      axisPointer: {
+        type: "line",
+        axis: "x",
+      },
+      backgroundColor: "#fff",
+      borderColor: "#ddd",
+      borderWidth: 1,
+      textStyle: {
+        fontFamily: "Arial",
+        fontSize: 12,
+        color: "#333",
+      },
+      extraCssText: "direction: rtl; text-align: right; padding: 10px;",
+    },
+    legend: {
+      right: 0,
+      align: "right",
+      itemGap: 20,
+      itemWidth: 15,
+      icon: "circle",
+      data: [
+        {
+          name: "16-19",
+        },
+        {
+          name: "12-15",
+        },
+        {
+          name: "5-11",
+        },
+        {
+          name: "0-4",
+        },
+      ],
+    },
+    grid: {
+      height: 160,
+      left: 60,
+      right: 50,
+      top: 45,
+      bottom: 60,
+    },
+    xAxis: {
+      name: "תאריך",
+      nameLocation: "middle",
+      nameTextStyle: {
+        color: "black",
+        fontSize: 14,
+        fontFamily: "Open Sans",
+        padding: [15, 0, 0, 0],
+      },
+      offset: 10,
+      type: "category",
+      data: [
+        "10-05-2025",
+        "11-05-2025",
+        "12-05-2025",
+        "13-05-2025",
+        "14-05-2025",
+        "15-05-2025",
+        "16-05-2025",
+        "17-05-2025",
+        "18-05-2025",
+        "19-05-2025",
+        "20-05-2025",
+        "21-05-2025",
+        "22-05-2025",
+        "23-05-2025",
+        "24-05-2025",
+        "25-05-2025",
+        "26-05-2025",
+        "27-05-2025",
+        "28-05-2025",
+        "29-05-2025",
+        "30-05-2025",
+        "31-05-2025",
+        "01-06-2025",
+        "02-06-2025",
+        "03-06-2025",
+        "04-06-2025",
+        "05-06-2025",
+        "06-06-2025",
+        "07-06-2025",
+        "08-06-2025",
+      ],
+      axisLine: {
+        lineStyle: {
+          color: "#ccc",
+        },
+      },
+      axisLabel: {
+        fontSize: 11,
+        fontFamily: "Arial",
+        color: "#555",
+      },
+    },
+    yAxis: {
+      name: "מספר מאומתים",
+      nameLocation: "middle",
+      nameTextStyle: {
+        color: "black",
+        fontSize: 14,
+        fontFamily: "Open Sans",
+        padding: [0, 0, 20, 0],
+      },
+
+      type: "value",
+      axisLine: {
+        show: false,
+      },
+      axisTick: {
+        show: false,
+      },
+      splitLine: {
+        lineStyle: {
+          color: "#eee",
+        },
+      },
+      axisLabel: {
+        fontSize: 11,
+        fontFamily: "Arial",
+        color: "#555",
+      },
+    },
+    series: [
+      {
+        name: "0-4",
+        type: "line",
+        data: [
+          "0",
+          "0",
+          "0",
+          "0",
+          "0",
+          "0",
+          "0",
+          "0",
+          "0",
+          "0",
+          "0",
+          "0",
+          "0",
+          "0",
+          "0",
+          "0",
+          "0",
+          "0",
+          "0",
+          "0",
+          "0",
+          "0",
+          "0",
+          "0",
+          "0",
+          "0",
+          "0",
+          "0",
+          "0",
+          "0",
+        ],
+        smooth: true,
+        symbol: "circle",
+        symbolSize: 6,
+        itemStyle: {
+          color: "#A88FEF",
+        },
+        lineStyle: {
+          color: "#A88FEF",
+          width: 2,
+        },
+      },
+      {
+        name: "5-11",
+        type: "line",
+        smooth: true,
+        symbol: "circle",
+        symbolSize: 6,
+        data: [
+          "0",
+          "0",
+          "0",
+          "0",
+          "0",
+          "0",
+          "0",
+          "0",
+          "0",
+          "0",
+          "0",
+          "0",
+          "0",
+          "0",
+          "0",
+          "0",
+          "0",
+          "0",
+          "0",
+          "0",
+          "0",
+          "0",
+          "0",
+          "0",
+          "0",
+          "0",
+          "0",
+          "0",
+          "0",
+          "0",
+        ],
+      },
+      {
+        name: "12-15",
+        type: "line",
+        smooth: true,
+        symbol: "circle",
+        symbolSize: 6,
+        data: [
+          "0",
+          "0",
+          "0",
+          "0",
+          "0",
+          "0",
+          "0",
+          "0",
+          "0",
+          "0",
+          "0",
+          "0",
+          "0",
+          "0",
+          "0",
+          "0",
+          "0",
+          "0",
+          "0",
+          "0",
+          "0",
+          "0",
+          "0",
+          "0",
+          "0",
+          "0",
+          "0",
+          "0",
+          "0",
+          "0",
+        ],
+      },
+      {
+        name: "16-19",
+        type: "line",
+        smooth: true,
+        symbol: "circle",
+        symbolSize: 6,
+        data: [
+          "0",
+          "0",
+          "0",
+          "0",
+          "0",
+          "0",
+          "0",
+          "0",
+          "0",
+          "0",
+          "0",
+          "0",
+          "0",
+          "0",
+          "0",
+          "0",
+          "0",
+          "0",
+          "0",
+          "0",
+          "0",
+          "0",
+          "0",
+          "0",
+          "0",
+          "0",
+          "0",
+          "0",
+          "0",
+          "0",
+        ],
+      },
+    ],
+  };
 
   return (
     <div
@@ -30,33 +316,6 @@ const GraphicalCard = ({ sectionId, card, hasContainerParent }) => {
         <div className="card__title font-base bold line-height-2xl">
           {graphicalCard.title}
         </div>
-
-        {graphicalCard.hasTimeRangeFilter && (
-          //         <select name="cars" id="cars">
-          //   <option value="volvo">Volvo</option>
-          //   <option value="saab">Saab</option>
-          //   <option value="opel">Opel</option>
-          //   <option value="audi">Audi</option>
-          // </select>
-          <div className="time-filter-table">
-            <select
-              onChange={(e) => {
-                const selectedTimeRange = e.target.value;
-                onChangeGraphDataTimeRange(
-                  sectionId,
-                  card.id,
-                  selectedTimeRange
-                );
-              }}
-            >
-              <option value={timeRanges.lastMonth}>חודש אחרון</option>
-              <option value={timeRanges.last3Months}>3 חודשים</option>
-              <option value={timeRanges.last6Months}>6 חודשים</option>
-              <option value={timeRanges.lastYear}>שנה אחרונה</option>
-              <option value={timeRanges.all}>הכל</option>
-            </select>
-          </div>
-        )}
 
         <button className="card__more-info_btn">
           <img src={moreInfoBtn} alt="more info" />
@@ -95,6 +354,10 @@ const GraphicalCard = ({ sectionId, card, hasContainerParent }) => {
           </ul>
         )}
       </div>
+
+      {graphicalCard.hasTimeRangeFilter && (
+        <TimeTableFilter sectionId={sectionId} cardId={card.id} />
+      )}
 
       <ReactECharts option={options} />
     </div>
